9aa56f274c98abd8556e0ca60f6cb37071e107e1
Daniel Cruz <156431265+daniewcruz@users.noreply.github.com>
2024-12-04 23:12:37 -0300

    Update doados.jsx

--- src/Pages/Doados/doados.jsx
+++ src/Pages/Doados/doados.jsx
@@ -1,20 +1,27 @@
 import S from "./doados.module.scss"
 import livro from "../../assets/livroProtagonista.png"
-export default function Doados() {
-    return (
-       <section className={S.boxDoados}>
-        <h2>Livros Doados</h2>
-        <section className={S.boxCard}>
-            <article>
-                <img src={livro} alt=" imagem de livro que se chama o protagonista" />
-                
-                <h3>O Protagonista</h3>
-                <p>Susanne Andrade
-                </p>
-                <p>Ficção</p>
-                
-            </article>
+export default function Doados(){
+    return(
+        <section className={S.boxDoados}>
+            <h2>Livros Doados</h2>
+            <section className={S.boxcard}>
+                <article>
+                    <img src={livro} alt="imagem de um livro que se chama o protagonista" />
+                    
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                    
+                </article>
+                <article>
+                    <img src={livro} alt="imagem de um livro que se chama o protagonista" />
+                    
+                    <h3>O Protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                    
+                </article>
+            </section>
         </section>
-       </section>
     )
 }
