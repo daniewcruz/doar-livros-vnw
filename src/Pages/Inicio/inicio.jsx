4550906bc476d0b873c3e27509b60f9c36b000f5
Daniel Cruz <156431265+daniewcruz@users.noreply.github.com>
2024-12-04 23:03:14 -0300

    Update inicio.jsx

--- src/Pages/Inicio/inicio.jsx
+++ src/Pages/Inicio/inicio.jsx
@@ -4,35 +4,36 @@ import leitura from "../../assets/leitura.png"
 import fornecer from "../../assets/fornecer.png"
 import balanco from "../../assets/balanco.png"
 
-export default function Inicio() {
+export default function Inicio(){
     return(
         <main>
-        <section className={S.post}>
-            <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
-        </section>
-        <section className={S.devoDoar}>
-            <div>
-                <h2>Por que devo doar?</h2>
-            </div>
-            <section className={S.boxCard}>
-                <article>
-                    <img src={comunicacao} alt="" />
-                    <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
-                </article>
-                <article>
-                    <img src={leitura} alt="" />
-                    <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
-                </article>
-                <article>
-                    <img src={fornecer} alt="" />
-                    <p>Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
-                </article>
-                <article>
-                    <img src={balanco} alt="" />
-                    <p>Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
-                </article>
+            <section className={S.post}>
+                <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
+            </section>
+            <section className={S.devoDoar}>
+                <div>
+                    <h2>Por que devo Doar?</h2>
+                </div>
+                <section className={S.boxCard}>
+                    <article>
+                            <img src={comunicacao} alt="" />
+                            <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
+                        </article> 
+                        <article>
+                            <img src={leitura} alt="" />
+                            <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
+                        </article>
+                        <article>
+                            <img src={fornecer} alt="" />
+                            <p>Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.</p>
+                        </article>
+                        <article>
+                            <img src={balanco} alt="" />
+                            <p>Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>
+                    </article>
             </section>
         </section>
-        </main>
+    </main>
+        
     )
 }
